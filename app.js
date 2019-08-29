a4df5d455797878a3424412c75036b2583d3f01b
limkaleb <limkaleb@gmail.com>
2019-08-29 13:49:45 +0700

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 // require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -26,6 +27,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
